88b631b212fce0effb1607aa73ff2ac5ddefb4db
RODRIGO DE LIMA <49422747+rodrigols-10@users.noreply.github.com>
2022-12-14 17:06:24 -0300

    fix bug

--- manager/connection.php
+++ manager/connection.php
@@ -1,8 +1,4 @@
 <?php
-    $usuario = "prodemadb";
-    $senha = "eUd*bGPEq4#";
-    $database = "prodemadb";
-    $host = "prodemadb.mysql.dbaas.com.br";
 
     // $usuario = "root";
     // $senha = "";
